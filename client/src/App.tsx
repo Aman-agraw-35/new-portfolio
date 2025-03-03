b490c97861c5910b806e48975377a0b3a46aa474
amanagraw35 <29628322-amanagraw35@users.noreply.replit.com>
2025-03-03 14:44:54 +0000

    sdddjdd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+// import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 import SocialMediaButton from "@/components/SocialMediaButton"; 
@@ -22,7 +22,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
-      <Toaster />
+      {/* <Toaster /> */}
       <SocialMediaButton /> 
       <RightFloatingButtons />
       <Footer />
